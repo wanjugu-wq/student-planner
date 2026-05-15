a962df170d55e147d530ee64048a7ed35df01c8c
samson kangara <samkangara06@gmail.com>
2026-05-15 10:08:03 +0300

    feat:add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import React from 'react'
+import CategoriesPage from "./pages/category";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div >
+      <CategoriesPage/>
+    </div>
+  )
 }
